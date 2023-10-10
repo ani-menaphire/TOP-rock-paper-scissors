32770535ab2a45f41a20236f381271b1d6ecf861
Ani <131550720+ani-menaphire@users.noreply.github.com>
2023-10-10 22:36:33 +0400

    Swap out buttons for images

--- script.js
+++ script.js
@@ -1,23 +1,13 @@
 const body = document.body
 const outerDiv = document.querySelector('.outer-div');
-const buttonDiv = document.querySelector('.button-div')
+const weaponsDiv = document.querySelector('.weapons')
 const startButton = document.querySelector('.start-button');
 startButton.addEventListener('click', game);
 const decision = document.querySelector('#decision');
 const score = document.querySelector('#score');
 const result = document.querySelector('#result');
 
-const rock = document.createElement('button');
-rock.textContent = 'Rock';
-rock.classList.add('rps-button');
-
-const paper = document.createElement('button');
-paper.textContent = 'Paper';
-paper.classList.add('rps-button');
-
-const scissors = document.createElement('button');
-scissors.textContent = 'Scissors';
-scissors.classList.add('rps-button');
+body.removeChild(outerDiv)
 
 let myScore = 0
 let computerScore = 0
@@ -34,23 +24,20 @@ function game() {
       }
     
       else if (answer == 'yes') {
-        
-        decision.textContent = 'Alright. So.. what will it be: Rock, Paper or Scissors?';
-        
+        body.appendChild(outerDiv)               
         body.setAttribute('id', 'body');
-        outerDiv.setAttribute('id', 'outer-div');
-        buttonDiv.setAttribute('id', 'button-div')
-        buttonDiv.append(rock);
         
-        buttonDiv.append(paper);
         
-        buttonDiv.append(scissors);
         
-        const buttons = document.querySelectorAll('.rps-button');
+        const buttons = document.querySelectorAll('.image-div');
 
         buttons.forEach(button => {
-          button.addEventListener('click', (e) => { 
-            playRound(e.target.textContent, restart);
+          let playerSelection = button.id.charAt(0).toUpperCase() + button.id.slice(1);
+          button.addEventListener('click', ()=> {        
+            if (myScore == 5 || computerScore == 5) {
+              return;
+            }
+            playRound(playerSelection, restart)
           })
         })
         
@@ -113,24 +100,20 @@ function playRound(playerSelection, restart) {
     computerScore++
   }   
   score.textContent = `You - ${myScore} : ${computerScore} - Me`;
-  if (myScore === 5 || computerScore === 5) {
-    rock.disabled = true
-    paper.disabled = true
-    scissors.disabled = true;
+  if (myScore === 5 || computerScore === 5) { 
     startButton.textContent = 'Click me again'
     body.append(startButton)
     startButton.removeEventListener('click', game)
     startButton.addEventListener('click', restart)
   }
-
+  
   if (myScore === 5) {
     result.textContent = 'Wait how did you.. That\'s not fair!! I wanna play again, click me click me click meeee.'
     
   }
   else if (computerScore === 5) {
     result.textContent = 'Haha yes, I beat you. I\'m so good at this- oh what\'s that? ...You wanna have your butt kicked again? if you say so, click me.'
-  }
- 
+  } 
 }
 
 
